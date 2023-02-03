3d4805cdab92ccaf9f2d749a7cbcf76fdbbd7b47
hanh085 <124012921+hanh085@users.noreply.github.com>
2023-02-03 16:10:53 +0700

    Update bai 10.js

--- bai 10.js
+++ bai 10.js
@@ -1,59 +1,52 @@
 // Bài 10: Viết chương trình nhập 1 số và kiểm tra có phải số fibonacci không?
 
-var n = 1*prompt('Nhập 1 số');  
-if (isFibonacci(n) == true)
-{
-    console.log('%i là số fibonacci',n); 
+var n = prompt('Nhập 1 số');
+while (Check(n) == false) {
+    n = prompt('Nhập 1 số');
 }
-else 
-    console.log('%i không là số fibonacci',n);  
+n = Number(n);
+
+if (isFibonacci(n) == true) {
+    console.log('%i là số fibonacci', n);
+}
+else
+    console.log('%i không là số fibonacci', n);
 
 
-function isFibonacci (n)
-{
-    if(Check(n) == false)
-    {
-        return false; 
+function isFibonacci(n) { 
+    if (n <= 0) {
+        return false;
     }
-    else
-    {
-        var a=1; 
-        var b=1; 
-        var k=0; 
-
-        while(k<n)
-        {
-            k = k+a+b; 
-            a=b; 
-            b=k; 
-        }
-        if (k == n)
-        {
-            return true; 
-        }
-        else return false; 
+    if (Check(n) == false) {
+        return false;
     }
-    
+    else {
+        var a = 1;
+        var b = 1;
+        var k = 0;
+
+        while (k < n) {
+            k = k + a + b;
+            a = b;
+            b = k;
+        }
+        if (k == n) {
+            return true;
+        }
+        else return false;
+    }
+
 }
 
 
-function Check(n)
-{
-    if(n === '' || n === null)
-    {
-        console.log('giá trị rỗng'); 
-        return false; 
+function Check(n) {
+    if (n === '' || n === null) {
+        console.log('giá trị rỗng');
+        return false;
     }
-    else if (isNaN(n))
-    {
-        console.log('giá trị ko là định dạng số'); 
-        return false; 
+    else if (isNaN(n)) {
+        console.log('giá trị ko là định dạng số');
+        return false;
     }
-    else if (n <= 0)
-    {
-        console.log('giá trị <= 0'); 
-        return false; 
-    }
-    
-    return true; 
+    return true;
 }
