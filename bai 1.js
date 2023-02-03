f93697d273718bf44a392e89f76f9b2bf477c042
hanh085 <124012921+hanh085@users.noreply.github.com>
2023-02-03 16:33:57 +0700

    Update bai 1.js

--- bai 1.js
+++ bai 1.js
@@ -1,39 +1,37 @@
 // bai 1: 
 // viết chương trình nhập chiều dài, chiều rộng của HCN, tính chu vi và diện tích
 
-var chDai = prompt('Nhập chiều dài của HCN: '); 
-var chRong = prompt('Nhập chiều rộng của HCN: '); 
+var chDai = prompt('Nhập chiều dài của HCN: ');
+var chRong = prompt('Nhập chiều rộng của HCN: ');
+while (Check(chDai) == false) {
+    chDai = prompt('Nhập chiều dài của HCN: ');
+}
+while (Check(chRong) == false) {
+    chRong = prompt('Nhập chiều rộng của HCN: ');
+}
+chDai = Number(chDai);
+chRong = Number(chRong);
 
-function Check(value)
-{
-    if(value === '' || value ===null)
-    {
-        console.log('giá trị chưa được nhập'); 
-        return false; 
+
+function Check(value) {
+    if (value === '' || value === null) {
+        console.log('giá trị chưa được nhập');
+        return false;
     }
-    else if(isNaN(value))
-    {
-        console.log('giá trị ko là định dạng số ! '); 
-        return false; 
+    else if (isNaN(value)) {
+        console.log('giá trị ko là định dạng số ! ');
+        return false;
     }
-    else if (value <= 0)
-    {
-        console.log('giá trị <= 0'); 
-        return false; 
+    else if (value <= 0) {
+        console.log('giá trị <= 0');
+        return false;
     }
-    return true; 
+    return true;
 }
 
-while (Check(chDai) == false)
-{
-    chDai = prompt('Nhập chiều dài của HCN: '); 
-}
-while (Check(chRong) == false)
-{
-    chRong = prompt('Nhập chiều rộng của HCN: '); 
-}
 
-console.log('Chieu dai = ',chDai); 
-console.log('Chieu rong = ',chRong); 
-console.log('Chu vi: ', 2*(1*chDai + 1*chRong)); 
-console.log('Dien tich: ', chDai*chRong); 
+
+console.log('Chieu dai = ', chDai);
+console.log('Chieu rong = ', chRong);
+console.log('Chu vi: ', 2 * (chDai + chRong));
+console.log('Dien tich: ', chDai * chRong); 
