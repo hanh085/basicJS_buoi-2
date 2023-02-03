12a27920519b05df390a8aa6a147975ba02c448d
hanh085 <124012921+hanh085@users.noreply.github.com>
2023-02-03 16:35:22 +0700

    Update bai 4.js

--- bai 4.js
+++ bai 4.js
@@ -2,55 +2,47 @@
 // giải pt bậc 1: ax + b = 0
 // (làm tròn kq đến 2 chữ só thập phân)
 
-alert('pt bac 1: ax+b = 0'); 
-var a = prompt('Nhap a: '); 
-var b = prompt('Nhap b: '); 
+alert('pt bac 1: ax+b = 0');
+var a = prompt('Nhap a: ');
+var b = prompt('Nhap b: ');
 
 // Kiểm tra a, b 
-while (Check(a) == false)
-{
-    a = prompt('Nhap a: ');  
+while (Check(a) == false) {
+    a = prompt('Nhap a: ');
 }
-while (Check(b) == false)
-{
-    b = prompt('Nhap b: ');  
+while (Check(b) == false) {
+    b = prompt('Nhap b: ');
 }
 
 // console.log(a); console.log(typeof(a));  
 // console.log(b); console.log(typeof(a));
-a= 1*a; // string -> number
-b= 1*b; 
+a = Number(a); // string -> number
+b = Number(b);
 // console.log(a); console.log(typeof(a));  
 // console.log(b); console.log(typeof(a));
 
 
-if (a != 0 && b!= 0)
-{
-    var x = -b/a; 
-    console.log('x = ', x.toFixed(2)); 
+if (a != 0 && b != 0) {
+    var x = -b / a;
+    console.log('x = ', x.toFixed(2));
 }
-else if (a== 0 && b==0)
-{
+else if (a == 0 && b == 0) {
     console.log('pt vo so nghiem')
 }
-else if (a== 0 && b!=0)
-{
+else if (a == 0 && b != 0) {
     console.log('pt vo nghiem')
-} 
+}
 
 
-function Check(value)
-{
-    if(value === '' || value === null)
-    {
-        console.log('giá trị rỗng'); 
-        return false; 
+function Check(value) {
+    if (value === '' || value === null) {
+        console.log('giá trị rỗng');
+        return false;
     }
-    else if (isNaN(value))
-    {
-        console.log('giá trị ko là định dạng số'); 
-        return false; 
+    else if (isNaN(value)) {
+        console.log('giá trị ko là định dạng số');
+        return false;
     }
 
-    return true; 
+    return true;
 }
